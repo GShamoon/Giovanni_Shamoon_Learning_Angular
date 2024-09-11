983abbd8a47cfc9cb059afab449fe9f073114cdd
GShamoon <w0775836@myscc.ca>
2024-09-11 18:56:25 -0400

    Created starter Angular file and complete Assignment 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'LearningAngular';
+  introString:string = "to me!";
+  myName = "Giovanni Shamoon";
+  // title = 'LearningAngular';
 }
