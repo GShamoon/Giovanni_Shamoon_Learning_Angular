bdbc3fa38fb347c54613278ebcaa429271bb50a1
GShamoon <w0775836@myscc.ca>
2024-10-01 23:02:12 -0400

    Added a method and added the service

--- src/app/bike-list/bike-list.component.ts
+++ src/app/bike-list/bike-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {Motorcycles} from "../Shared/Modules/motorcycles";
 import {BikeListItemComponent} from "../bike-list-item/bike-list-item.component";
 import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {MotorcycleService} from "../motorcycle.service";
 
 @Component({
   selector: 'app-bike-list',
@@ -13,9 +14,11 @@ import {NgClass, NgForOf, NgIf} from "@angular/common";
   styleUrl: './bike-list.component.css'
 })
 export class BikeListComponent {
-  bike4: Motorcycles = {id: 4, make: "Kawasaki", model: "Ninja H2R", year: 2016, streetLegal: false};
-  bike5: Motorcycles = {id: 5, make: "Trident", model: "Trident 660", year: 2023, streetLegal: true};
-  bike6: Motorcycles = {id: 6, make: "Aprilia", model: "RSV4 RR", year: 2024, streetLegal: true};
-  bike7: Motorcycles = {id: 7, make: "Arch", model: "Method143", year: 2025, streetLegal: false};
-  bikeList: Motorcycles [] = [this.bike4, this.bike5, this.bike6, this.bike7];
+  constructor(private MotorcycleService: MotorcycleService) {
+  }
+  // bike4: Motorcycles = {id: 4, make: "Kawasaki", model: "Ninja H2R", year: 2016, streetLegal: false};
+  // bike5: Motorcycles = {id: 5, make: "Trident", model: "Trident 660", year: 2023, streetLegal: true};
+  // bike6: Motorcycles = {id: 6, make: "Aprilia", model: "RSV4 RR", year: 2024, streetLegal: true};
+  // bike7: Motorcycles = {id: 7, make: "Arch", model: "Method143", year: 2025, streetLegal: false};
+  // bikeList: Motorcycles [] = [this.bike4, this.bike5, this.bike6, this.bike7];
 }
--- src/app/motorcycle.service.ts
+++ src/app/motorcycle.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {Motorcycles} from "./Shared/Modules/motorcycles";
+import {bikeList} from "./Shared/mock-motorcycles";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MotorcycleService {
-
+  private bikeList: Motorcycles[] = bikeList;
   constructor() { }
+
+  getBikeList(): Observable<Motorcycles[]>{
+    return of(bikeList)
+  }
 }
