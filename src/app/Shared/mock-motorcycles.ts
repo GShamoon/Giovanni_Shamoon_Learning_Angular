2ecc5964bb9ac96c2966893440d8f49f53226f7d
GShamoon <w0775836@myscc.ca>
2024-10-01 22:49:51 -0400

    Created mock content and a service

---
+++ src/app/Shared/mock-motorcycles.ts
@@ -0,0 +1,10 @@
+import {Motorcycles} from "./Modules/motorcycles";
+
+export const bikeList: Motorcycles[] = [
+  {id: 1, make: "Ducati", model: "Panigale V4", year: 2020, streetLegal: true},
+  {id: 2, make: "Yamaha", model: "R6", year: 2006, streetLegal: true},
+  {id: 3, make: "BMW", model: "K75", year: 1991, streetLegal: true},
+  {id: 4, make: "Kawasaki", model: "Ninja H2R", year: 2016, streetLegal: false},
+  {id: 5, make: "Trident", model: "Trident 660", year: 2023, streetLegal: true},
+  {id: 6, make: "Aprilia", model: "RSV4 RR", year: 2024, streetLegal: true},
+  {id: 7, make: "Arch", model: "Method143", year: 2025, streetLegal: false}];
