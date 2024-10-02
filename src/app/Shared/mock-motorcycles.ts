eae940a27e710e2d50dea86e681b8ea620a009d4
Giovanni Shamoon <w0775836@myscc.ca>
2024-10-02 13:14:46 -0400

    Refactored one of the bike makes as it was incorrect

--- src/app/Shared/mock-motorcycles.ts
+++ src/app/Shared/mock-motorcycles.ts
@@ -5,6 +5,6 @@ export const bikeList: Motorcycles[] = [
   {id: 2, make: "Yamaha", model: "R6", year: 2006, streetLegal: true},
   {id: 3, make: "BMW", model: "K75", year: 1991, streetLegal: true},
   {id: 4, make: "Kawasaki", model: "Ninja H2R", year: 2016, streetLegal: false},
-  {id: 5, make: "Trident", model: "Trident 660", year: 2023, streetLegal: true},
+  {id: 5, make: "Triumph", model: "Trident 660", year: 2023, streetLegal: true},
   {id: 6, make: "Aprilia", model: "RSV4 RR", year: 2024, streetLegal: true},
   {id: 7, make: "Arch", model: "Method143", year: 2025, streetLegal: false}];
