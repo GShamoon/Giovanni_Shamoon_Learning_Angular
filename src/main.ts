de5b8db46824db8142cfd4e2af0d9c539e1a6fed
Giovanni Shamoon <w0775836@myscc.ca>
2024-10-08 14:49:47 -0400

    Created a list of routes and attached them to the bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {BikeListComponent} from "./app/bike-list/bike-list.component";
+import {BikeListItemComponent} from "./app/bike-list-item/bike-list-item.component";
+import {provideRouter, Routes} from "@angular/router";
+const routes: Routes = [
+  {path:'', redirectTo: '/bikes', pathMatch: 'full'}, //default route
+  { path: 'bikes', component: BikeListComponent },
+  { path: 'bikes/:id', component: BikeListItemComponent }]
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {providers:[provideRouter(routes)]})
   .catch((err) => console.error(err));
