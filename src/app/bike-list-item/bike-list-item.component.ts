6d865f378bc6cc3ad44082f6e87ebb9ca8d37d84
Giovanni Shamoon <w0775836@myscc.ca>
2024-11-15 22:48:40 -0500

    Implemented standard pipes into items

--- src/app/bike-list-item/bike-list-item.component.ts
+++ src/app/bike-list-item/bike-list-item.component.ts
@@ -1,12 +1,16 @@
 import {Component, Input} from '@angular/core';
 import {Motorcycles} from "../Shared/Modules/motorcycles";
-import {NgOptimizedImage} from "@angular/common";
+import {DatePipe, LowerCasePipe, NgIf, NgOptimizedImage, UpperCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-bike-list-item',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgIf,
+    UpperCasePipe,
+    LowerCasePipe,
+    DatePipe
   ],
   templateUrl: './bike-list-item.component.html',
   styleUrl: './bike-list-item.component.css'
