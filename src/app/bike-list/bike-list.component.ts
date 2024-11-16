c68a9b8fe655851144486dd9b60d40bbe707fd09
Giovanni Shamoon <w0775836@myscc.ca>
2024-11-15 21:14:00 -0500

    Added buttons for editing and deleting along with form for the items

--- src/app/bike-list/bike-list.component.ts
+++ src/app/bike-list/bike-list.component.ts
@@ -3,19 +3,21 @@ import {Motorcycles} from "../Shared/Modules/motorcycles";
 import {BikeListItemComponent} from "../bike-list-item/bike-list-item.component";
 import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {MotorcycleService} from "../motorcycle.service";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-bike-list',
   standalone: true,
   imports: [
-    BikeListItemComponent, NgForOf, NgIf, NgClass
+    BikeListItemComponent, NgForOf, NgIf, NgClass, RouterLink
   ],
   templateUrl: './bike-list.component.html',
   styleUrl: './bike-list.component.css'
 })
 export class BikeListComponent {
   bikeList: Motorcycles [] = [];
-  constructor(private motorcycleService: MotorcycleService) {
+  constructor(private motorcycleService: MotorcycleService,
+              private router:Router) {
   }
 
   ngOnInit(){
@@ -24,6 +26,14 @@ export class BikeListComponent {
       error: err => console.error("There was an error fetching Motorcycles"),
       complete: () => console.log("Motorcycle data fetch complete")
     })
+  }
+
+  onEdit(){
+    this.router.navigate(["modify-bike"])
+  }
+
+  onDelete(){
+
   }
   // bike4: Motorcycles = {id: 4, make: "Kawasaki", model: "Ninja H2R", year: 2016, streetLegal: false};
   // bike5: Motorcycles = {id: 5, make: "Trident", model: "Trident 660", year: 2023, streetLegal: true};
--- src/app/modify-bike/modify-bike.component.ts
+++ src/app/modify-bike/modify-bike.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MotorcycleService} from "../motorcycle.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-modify-bike',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './modify-bike.component.html',
   styleUrl: './modify-bike.component.css'
 })
 export class ModifyBikeComponent {
-
+  bikeForm: FormGroup;
+  constructor(private fb: FormBuilder,
+              private MotorcycleService:MotorcycleService, private router:Router) {
+    this.bikeForm = this.fb.group({
+      id: ['', Validators.required],
+      make: ['', Validators.required],
+      model: ['', Validators.required],
+      year: ['', Validators.required],
+      streetLegal: [false]
+    });
+  }
 }
